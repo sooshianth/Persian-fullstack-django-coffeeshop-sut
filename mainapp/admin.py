8df44992face5ff0b7188ccdee6f0da9914a9bcf
Tyrmo <itsTyrmo@hotmail.com>
2024-06-13 21:06:40 +0330

    created users and orders models with the linkations needed

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Product, RawMaterial, ProductRawMaterial
+from .models import Product, RawMaterial, ProductRawMaterial, Order, OrderProduct, CustomerAccount
+
+class CustomerAccountAdmin(admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'datetime_signed_up']
 
 class ProductRawMaterialInline(admin.TabularInline):
     model = ProductRawMaterial
+    extra = 1
+
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
     extra = 1
 
 class ProductAdmin(admin.ModelAdmin):
@@ -12,6 +19,11 @@ class ProductAdmin(admin.ModelAdmin):
 class RawMaterialAdmin(admin.ModelAdmin):
     list_display = ['name', 'stock']
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'customer', 'order_date']
+    inlines = (OrderProductInline,)
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(RawMaterial)
+admin.site.register(CustomerAccount, CustomerAccountAdmin)
--- mainapp/models.py
+++ mainapp/models.py
@@ -33,15 +33,31 @@ class ProductRawMaterial(models.Model):
         return f"{self.quantity} of {self.raw_material.name} for {self.product.name}"
 
 
-# class CustomerAccount(models.Model):
-#     user = models.CharField(max_length=255) # either user or email
-#     password = models.CharField(max_length=255, validators=[validators.RegexValidator(r'^[0-9a-zA-Z ]+$')])
-#     first_name = models.CharField(max_length=255)
-#     last_name = models.CharField(max_length=255)
-#     datetime_signed_up = models.DateTimeField(auto_now_add=True)
-#     current_cart = []
-#     all_products_ever = []
+class CustomerAccount(models.Model):
+    user = models.CharField(max_length=255) # either user or email
+    password = models.CharField(max_length=255, validators=[validators.RegexValidator(r'^[0-9a-zA-Z ]+$')])
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    datetime_signed_up = models.DateTimeField(auto_now_add=True)
+    current_cart = []
+    all_products_ever = []
 
-#     def __str__(self):
-#         return self.user
+    def __str__(self):
+        return self.user
 
+class Order(models.Model):
+    customer = models.ForeignKey(CustomerAccount, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='OrderProduct')
+    order_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return str(self.pk)
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1, validators=[validators.MinValueValidator(1)])
+
+    def __str__(self) -> str:
+        return f"{self.quantity} of {self.product.name} for {self.order.customer}"
